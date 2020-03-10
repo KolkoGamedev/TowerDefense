6b87b4a6d8c3519099e0796d4d7babc8d216cc15
Syberian <mixux98@gmail.com>
2020-03-10 10:22:07 +0100

    added baseTurretGround

---
+++ Assets/Scripts/Tester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Turrets;
+using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    [SerializeField] private BaseTurret baseTurret = null;
+
+    private void Start()
+    {
+        Debug.Log((baseTurret.damage * baseTurret.rateOfFire)/60);
+    }
+}
---
+++ Assets/Turrets/BaseTurret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Turrets
+{ 
+    [CreateAssetMenu] 
+    public class BaseTurret : ScriptableObject
+    {
+        public float damage = 5f;
+        public float rateOfFire = 60f;
+        public Element turretElement = Element.Wind;
+    }
+}
+
+public enum Element
+{
+    Fire,
+    Cold,
+    Water,
+    Wind,
+    Earth,
+    Neutral
+}
