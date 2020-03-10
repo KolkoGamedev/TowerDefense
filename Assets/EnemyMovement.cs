d863cf00a5a033a29d3552fa6ddcaecc2aced263
DawidR2 <61984245+DawidR2@users.noreply.github.com>
2020-03-10 11:44:22 +0100

    Player movement + waypoints

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime,Space.World);
+
+        if(Vector3.Distance(transform.position,target.position)<= 0.1f)
+        {
+            GetNextWaypoint();
+        }
+        
+    }
+    void GetNextWaypoint()
+    {
+        if(wavepointIndex >= Waypoints.waypoints.Length-1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.waypoints[wavepointIndex];
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] waypoints;
+
+     void Awake()
+    {
+        waypoints = new Transform[transform.childCount];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i]=transform.GetChild(i);
+        }
+    }
+
+}
