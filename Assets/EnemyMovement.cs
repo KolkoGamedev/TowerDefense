2c6bd67318f80d745a99cc0679b92afa5932702b
DawidR2 <61984245+DawidR2@users.noreply.github.com>
2020-03-10 12:14:10 +0100

    Fix

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -3,21 +3,21 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] float speed;
+    [SerializeField] private float _speed;
 
-    private Transform target;
-    private int wavepointIndex = 0;
+    private Transform _target;
+    private int _wavepointIndex = 0;
 
     private void Start()
     {
-        target = Waypoints.waypoints[0];
+        _target = Waypoints.waypoints[0];
     }
     private void Update()
     {
-        Vector3 direction = target.position - transform.position;
-        transform.Translate(direction.normalized * speed * Time.deltaTime,Space.World);
+        Vector3 direction = _target.position - transform.position;
+        transform.Translate(direction.normalized * (_speed * Time.deltaTime),Space.World);
 
-        if(Vector3.Distance(transform.position,target.position)<= 0.1f)
+        if(Vector3.Distance(transform.position,_target.position)<= 0.1f)
         {
             GetNextWaypoint();
         }
@@ -25,12 +25,12 @@ public class EnemyMovement : MonoBehaviour
     }
     void GetNextWaypoint()
     {
-        if(wavepointIndex >= Waypoints.waypoints.Length-1)
+        if(_wavepointIndex >= Waypoints.waypoints.Length-1)
         {
             Destroy(gameObject);
             return;
         }
-        wavepointIndex++;
-        target = Waypoints.waypoints[wavepointIndex];
+        _wavepointIndex++;
+        _target = Waypoints.waypoints[_wavepointIndex];
     }
 }
